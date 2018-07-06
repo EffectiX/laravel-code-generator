b6b0432a2549cda42d57ec5ff2685db3caee44ea
Jorge Morales <jlm@effectix.net>
2018-07-06 12:45:32 -0400

    WIP

---
+++ src/CodeGeneratorServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+namespace Effectix\CodeGen;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Code Generator Service Provider class
+ */
+class CodeGeneratorServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
