4b9758351983f6adb5bf71ea943c029a6f1c1f80
Jorge Morales <jlm@effectix.net>
2018-07-08 13:05:01 -0400

    Write out a basic config file with a couple of options...

---
+++ config/laravel-code-generator.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'length' => 5,
+    'character_pool' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_',
+    'new-key' => 'some-value',
+];
--- src/CodeGeneratorServiceProvider.php
+++ src/CodeGeneratorServiceProvider.php
@@ -10,11 +10,15 @@ class CodeGeneratorServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/../config/laravel-code-generator.php' => config_path('laravel-code-generator.php'),
+        ], 'codegen-config');
     }
 
     public function register()
     {
-        //
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/laravel-code-generator.php', 'codegen-config'
+        );
     }
 }
